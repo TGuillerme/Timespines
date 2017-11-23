849801379d6b6465c9a8b0dc1a35d3f33368362c
Thomas Guillerme <guillert@tcd.ie>
2017-11-23 15:56:45 +1000

    added plot function

--- Functions/clade.change_fun.R
+++ Functions/clade.change_fun.R
@@ -84,3 +84,123 @@ extract.change <- function(list, what, inc.null = FALSE) {
     }
 }
 
+#' @title Plots changes
+#'
+#' @description Plots the data change for a single group or a list of groups
+#'
+#' @param group.data A vector or a list
+#' @param change.data A vector or a list
+#' @param histogram logical, whether to plot the histogram in the background (TRUE)
+#' @param scale.density logical, whether to scale the density (TRUE)
+#' @param change.hist logical, whether to display the changes a histogram (TRUE)
+#' @param ... Arguments to be passed to plot()
+#'
+#' @examples
+#' 
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+
+plot.change <- function(group.data, change.data, histogram = TRUE, scale.density = TRUE, change.hist = TRUE, ...) {
+
+    ## Function for one single plot
+    plot.distribution <- function(group.data, histogram, scale.density, ...) {
+        group_data <- as.numeric(group.data)
+
+        ## Get the histogram data
+        histogram_data <- hist(group_data, plot = FALSE)
+        density_data <- density(group_data, na.rm = TRUE)
+
+        if(scale.density) {
+            ## Scaling the frequency/density
+            histogram_data$density <- histogram_data$density/max(histogram_data$density)
+            density_data$y <- density_data$y/max(density_data$y)
+        }
+
+        if(histogram) {
+            ## Plot the histogram
+            plot(histogram_data, border = "grey", yaxt = "n", ...)
+            # plot(histogram_data, border = "grey", yaxt = "n") ; warning("DEBUG")
+            ## Ticks on the y axis
+            ticks <- axTicks(2)
+            ## Labels on the y axis
+            if(scale.density) {
+                ## Scale the tick labels
+                ticks_labels <- ticks/max(histogram_data$counts)
+
+                ## Get the rounding parameters
+                rounding <- 2
+                if(max(ticks_labels) > 1){
+                    rounding <- 1
+                }
+                if(max(ticks_labels) > 10){
+                    rounding <- 0
+                }
+
+                ## Round the tip labels
+                ticks_labels <- round(ticks_labels, digit = rounding)
+            } else {
+                ticks_labels <- ticks
+            }
+            ## Labels on the y axis
+            axis(2, labels = ticks_labels, at = ticks)
+
+            ## Scaling the density line
+            density_data$y <- density_data$y * max(histogram_data$counts)
+
+            ## Add the density
+            lines(density_data)
+        } else {
+            ## Plot the density
+            plot(density_data$x, density_data$y, type = "l", ...)
+            # plot(density_data$x, density_data$y, type = "l") ; warning("DEBUG")
+        }
+    }
+
+    plot.change.occurence <- function(change.data, group.data, change.hist, scale.density, ...) {
+        change_data <- as.numeric(change.data)
+        group_data <- as.numeric(group.data)
+
+        ## Get the histogram data (for scaling)
+        histogram_data <- hist(group_data, plot = FALSE)
+        density_data <- density(group_data, na.rm = TRUE)
+
+        if(scale.density) {
+            ## Scaling the frequency/density
+            histogram_data$density <- histogram_data$density/max(histogram_data$density)
+            density_data$y <- density_data$y/max(density_data$y)
+        }
+
+        if(change.hist) {
+            ## Getting the histogram data
+            histogram_change <- hist(change_data, plot = FALSE)
+            density_change <- density(change_data, na.rm = TRUE)
+            ## Plotting the histogram results
+            plot(histogram_change, col = "lightgrey", border = "darkgrey", ...)
+            lines(density_change, lty = 2)
+
+        } else {
+            ## Scaling the density line
+            density_data$y <- density_data$y * max(histogram_data$counts)
+
+            ## Add segments below the density line
+            for(one_change in 1:length(change_data)) {
+                x_value <- narrow.down.x(change_data[one_change], density_data$x)
+                segments(x0 = density_data$x[x_value], y0 = 0, y1 = density_data$y[x_value], lwd = 1)
+            }
+
+        }
+
+    }
+            
+    if(class(group.data) != "list") {
+        ## Plot the overall distribution
+        plot.distribution(group.data, histogram = histogram, scale.density = scale.density, ...)
+
+        ## Add the changes
+        plot.change.occurence(change.data, group.data, change.hist = change.hist, scale.density = scale.density, ...)
+    }
+
+}
